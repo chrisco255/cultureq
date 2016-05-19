cae22568bc9710d7012e5e7c5e99747c43e005fd
Ben Diuguid <bendiuguid@gmail.com>
2016-05-19 11:42:17 -0400

    quick rename

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import Home from './containers/HomePage';
-import SignUp from './containers/SignUpPage';
-import Dashboard from './containers/DashboardPage';
+import HomePage from './containers/HomePage';
+import SignUpPage from './containers/SignUpPage';
+import DashboardPage from './containers/DashboardPage';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Home} />
-    <Route path="signup" component={SignUp} />
-    <Route path="dashboard" component={Dashboard} />
+    <IndexRoute component={HomePage} />
+    <Route path="signup" component={SignUpPage} />
+    <Route path="dashboard" component={DashboardPage} />
   </Route>
 )
