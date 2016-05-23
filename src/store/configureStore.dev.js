9d94e1a87461fe9e3e6703aa95ec798454ae7b00
chrisco <ufcordle@hotmail.com>
2016-05-23 15:22:50 -0400

    Chrome Devtools extension support for Redux Devtool

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -4,26 +4,26 @@ import reducer from '../reducers/index';
 import DevTools from '../approot/DevTools.component.js';
 
 export default function configureStore(initialState) {
-    const sagaMiddleware = createSagaMiddleware();
+	const sagaMiddleware = createSagaMiddleware();
 
-    const store = createStore(
-        reducer,
-        initialState,
-        compose(
-            applyMiddleware(sagaMiddleware),
-            DevTools.instrument()
-        )
-    );
+	const store = createStore(
+		reducer,
+		initialState,
+		compose(
+			applyMiddleware(sagaMiddleware),
+			window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument()
+		)
+	);
 
-    if (module.hot) {
-        // Enable Webpack hot module replacement for reducers
-        module.hot.accept('../reducers/index', () => {
-            const nextRootReducer = require('../reducers/index').default;
-            store.replaceReducer(nextRootReducer);
-        });
-    }
+	if (module.hot) {
+		// Enable Webpack hot module replacement for reducers
+		module.hot.accept('../reducers/index', () => {
+			const nextRootReducer = require('../reducers/index').default;
+			store.replaceReducer(nextRootReducer);
+		});
+	}
 
-    store.runSaga = sagaMiddleware.run;
-    store.close = () => store.dispatch(END);
-    return store;
+	store.runSaga = sagaMiddleware.run;
+	store.close = () => store.dispatch(END);
+	return store;
 };
