9c18d0451cf831898900b2d7868f754bacf5786f
chrisco <ufcordle@hotmail.com>
2016-05-23 17:08:52 -0400

    comments

--- src/routes/SignUp/SignUp.valid.js
+++ src/routes/SignUp/SignUp.valid.js
@@ -1,5 +1,5 @@
 import { createValidator, required, maxLength, minLength, integer } from '../../common/validations';
-
+//yay
 const signUpValidation = createValidator({
   companyName: [required, minLength(4), maxLength(10)],
   address: [required, integer, minLength(7)]
