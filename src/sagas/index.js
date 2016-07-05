f5f7fc75e92e64fd7e002ddf5eae15e09cfc6597
Ben Diuguid <bendiuguid@gmail.com>
2016-07-05 11:38:27 -0400

    Removed product/proposal stuff

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,15 +1,12 @@
 import { put, call, fork } from 'redux-saga/effects';
 import { takeEvery, delay } from 'redux-saga';
 import { watchFetchCompanies, watchCompanySubmitted } from './company/Company.saga';
-import { watchFetchProducts, watchSubmitProduct } from './product/Product.saga';
 import watchPillarListSubmitted from './pillar/Pillar.saga';
 
 export default function* rootSaga() {
   yield [
   	watchFetchCompanies(),
     watchPillarListSubmitted(),
-    watchCompanySubmitted(),
-    watchFetchProducts(),
-    watchSubmitProduct()
+    watchCompanySubmitted()
   ];
 }
