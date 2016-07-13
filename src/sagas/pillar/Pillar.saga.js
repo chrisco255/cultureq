44eb9637a3e0b09791484db034d5382d12a924cb
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2016-07-13 15:56:19 -0400

    pillar saga handles pillar create and delete

--- src/sagas/pillar/Pillar.saga.js
+++ src/sagas/pillar/Pillar.saga.js
@@ -1,31 +1,43 @@
 import { put, call, fork } from 'redux-saga/effects';
 import { takeEvery, delay } from 'redux-saga';
 import axios from 'axios';
-
 import { push } from 'react-router-redux';
-
 import * as ActionTypes from '../../reducers/pillar/Pillar.actions';
 
 const post = (body) => {
-	console.log("Posting create pillar body - ", body);
+	console.log("Posting pillar body - ", body);
 	return axios.post('/api/pillar', body).then( response => response.data );
 }
 
-export function* addPillarList(action) {
+export function* pillarCreate(action) {
 	try {
-		console.log('in saga');
 		const payload = yield call(post, {
-			type: 'command.ADD_PILLAR_LIST',
-			payload: action.payload
+			type: 'command.PILLAR_CREATE',
+			payload: action.payload.pillar
 		});
 		console.log('received payload from call - ', payload);
-		yield put( {type: ActionTypes.ADD_PILLAR_LIST_SUCCEEDED, payload } );
-		yield put( push('/content') );
+		yield put( {type: ActionTypes.PILLAR_CREATE_SUCCEEDED, payload } );
 	} catch (error) {
-		yield put( {type: ActionTypes.ADD_PILLAR_LIST_FAILED, error} );
+		yield put( {type: ActionTypes.PILLAR_CREATE_FAILED, error} );
 	}
 }
+export function* watchPillarCreateSubmitted() {
+	yield* takeEvery(ActionTypes.PILLAR_CREATE_SUBMITTED, pillarCreate);
+}
 
-export default function* watchPillarListSubmitted() {
-	yield* takeEvery(ActionTypes.ADD_PILLAR_LIST, addPillarList);
+export function* pillarDelete(action) {
+	try {
+		console.log('inside saga - pillarDelete');
+		const payload = yield call(post, {
+			type: 'command.PILLAR_DELETE',
+			payload: action.payload.pillar
+		});
+		console.log('received payload from call - ', payload);
+		yield put( {type: ActionTypes.PILLAR_DELETE_SUCCEEDED, payload } );
+	} catch (error) {
+		yield put( {type: ActionTypes.PILLAR_DELETE_FAILED, error} );
+	}
+}
+export function* watchPillarDeleteSubmitted() {
+	yield* takeEvery(ActionTypes.PILLAR_DELETE_SUBMITTED, pillarDelete);
 }
