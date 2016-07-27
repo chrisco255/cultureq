c799ffb4831ee15fbb1c8a9b3fad38d829ddd2f8
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2016-07-27 09:46:57 -0400

    cards for content

--- src/routes/content/ContentPage.container.js
+++ src/routes/content/ContentPage.container.js
@@ -5,7 +5,7 @@ import CSSModules from 'react-css-modules';
 import ContentPageStyles from './ContentPage.css';
 import { Link, IndexLink } from 'react-router';
 import _ from 'lodash';
-import { createContent, fetchContents } from '../../reducers/content/Content.actions';
+import { createContent, deleteContent, fetchContents } from '../../reducers/content/Content.actions';
 import ContentForm from './content_form/ContentForm.component';
 import { fetchPillars } from '../../reducers/pillar/Pillar.actions';
 
@@ -21,10 +21,11 @@ const pillarQuery = `
 
 const contentQuery = `
 {
-	contents {
-	  _id
-	  pillarId
-	  type
+  contents {
+    _id
+    pillarId
+    type
+    isDeleted
     data {
       title
       description
@@ -34,8 +35,7 @@ const contentQuery = `
       recipient
       recipientPosition
     }
-    isDeleted
-	}
+  }
 }
 `;
 
@@ -43,8 +43,11 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		createContent: (content) =>
 			dispatch(createContent(content)),
-		onLoad: () => {
-			dispatch(fetchPillars({ query:pillarQuery }))
+    deleteContent: (content) =>
+      dispatch(deleteContent(content)),
+		onLoad: () =>
+    {
+      dispatch(fetchPillars({ query:pillarQuery }))
       dispatch(fetchContents({ query:contentQuery }))
     }
 	}
@@ -57,14 +60,6 @@ const mapStateToProps = (state) => {
 		isEditing: state.content.isEditing
 	}
 }
-
-var theEditContent = null;
-
-$(document).ready(function(){
-	$('.collapsible').collapsible({
-		accordion : false
-	});
-});
 
 class ContentPage extends Component {
 
@@ -92,18 +87,43 @@ class ContentPage extends Component {
 
 		var listContents = null;
     var createContentListClassName = null;
+    var activeContents = null;
 
-    if(this.props.contents > 0) {
+    activeContents = this.props.contents.filter((content) => !content.isDeleted);
+
+    if(activeContents.length > 0) {
       createContentListClassName = "col s6";
       listContents = this.props.contents.map((content, index) => {
-				return (
-					<li key={content._id}>
-     				<div className="collapsible-header">Content #{index + 1}</div>
-						<div className="collapsible-body">
-							<p>Content type: {content.type}</p>
-						</div>
-     			</li>
-				)
+        if(!content.isDeleted) {
+          return (
+              <div className="card blue-grey darken-1" key={content._id}>
+                <div className="card-content white-text">
+                  <span className="card-title">Content #{index + 1}</span>
+                  <p>Linked to Pillar: {this.props.pillars[_.findIndex(this.props.pillars, (pillar) => pillar._id === content.pillarId)].name}</p>
+                  {content.type === 'QUOTE' &&
+                  <div>
+                    <p>Quote: {content.data.quote}</p>
+                    <p>Author: {content.data.author}</p>
+                  </div>}
+                  {content.type === 'VIDEO' &&
+                  <div>
+                    <p>Title: {content.data.title}</p>
+                    <p>Description: {content.data.description}</p>
+                    <p>URL: {content.data.url}</p>
+                  </div>}
+                  {content.type === 'LUNCH' &&
+                  <div>
+                    <p>Recipient: {content.data.recipient}</p>
+                    <p>Position: {content.data.recipientPosition}</p>
+                  </div>}
+                </div>
+                <div className="card-action" styleName="flex-space-between">
+                  <a className="hand">Edit</a>
+                  <a className="hand" onClick={this.props.deleteContent.bind(this, content)}><i className="material-icons">delete</i></a>
+                </div>
+              </div>
+  				);
+        }
 			});
     } else {
       createContentListClassName = "col s12";
@@ -128,15 +148,11 @@ class ContentPage extends Component {
       			</div>
 			    </div>
 
-          { this.props.contents.length > 0 && <div className="col s6">
+          { activeContents.length > 0 && <div className="col s6">
             <div className="container">
               <h1 styleName="title">Your Content</h1>
               <hr/>
-              <div>
-                <ul className="collapsible" data-collapsible="accordion">
-                 {listContents}
-                </ul>
-             </div>
+              <div>{listContents}</div>
             </div>
           </div> }
 
