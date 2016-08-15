3984a4877d32b5214fda427544dea06b239eae32
Cason Clagg <casonclagg@gmail.com>
2016-08-15 13:49:57 -0500

    Highcharts + Culture Points

--- src/routes/analytics/AnalyticsPage.container.js
+++ src/routes/analytics/AnalyticsPage.container.js
@@ -4,6 +4,7 @@ import styles from './AnalyticsPage.css';
 import { connect } from 'react-redux';
 import { fetchAnalytics } from '../../reducers/analytics/Analytics.actions';
 import AnalyticCard from '../../components/cards/Analytic.component';
+import ReactHighcharts from 'react-highcharts';
 
 const query = `
   {
@@ -15,9 +16,15 @@ const query = `
       totalPoints
       pillarCount
       contentCount
+      pointsByDate {
+        date
+        count
+      }
     }
   }
 `;
+
+const afterRender = (chart) => { /* do stuff with the chart  */ };
 
 const mapDispatchToProps = (dispatch) => ({
 	onLoad() {
@@ -25,9 +32,11 @@ const mapDispatchToProps = (dispatch) => ({
   }
 });
 
-const mapStateToProps = (state) => ({
-	analytics: state.analytics
-});
+const mapStateToProps = (state) => {
+    return {
+    	analytics: state.analytics
+    }
+}
 
 class Page extends Component {
 
@@ -36,95 +45,89 @@ class Page extends Component {
 	}
 
 	render() {
-    const { analytics } = this.props;
+        const { analytics, pointData } = this.props;
 
-    if(this.props.analytics) {
-      return (
-        <div className="container">
+        if(this.props.analytics) {
+            const config = {
+                title: {
+                    text: "Culture Points",
+                },
+                xAxis: {
+                    title: {
+                        text: "Month",
+                    },
+                    type: 'datetime'
+                },
+                yAxis: {
+                    title: {
+                        text: "Points",
+                    }
+                },
+                series: [{
+                    type: 'line',
+                    name: 'All',
+                    data: analytics.pointsByDate ? analytics.pointsByDate.map((x) => {
+                                return [new Date(x.date).getTime(), x.count]
+                            }) : []
+                }]
+            };
+          return (
+            <div className="container">
 
-          <div styleName="card-container">
-            <AnalyticCard
-              title="Toxic Practices"
-              isGood={true}
-              isPercentage={false}
-              value={3}
-              valueChange={-8}
-              // valueOutOf={}
-              valueUnits="issues"
-            />
+              <ReactHighcharts config={config} callback={afterRender}>
+              </ReactHighcharts>
 
-            <AnalyticCard
-              title="Turnovers"
-              isGood={false}
-              isPercentage={true}
-              value={12}
-              valueChange={8}
-              // valueOutOf={}
-              // valueUnits=""
-            />
+              <div styleName="card-container">
+                <AnalyticCard
+                  title="Toxic Practices"
+                  isGood={true}
+                  isPercentage={false}
+                  value={3}
+                  valueChange={-8}
+                  // valueOutOf={}
+                  valueUnits="issues"
+                />
 
-            <AnalyticCard
-              title="Completion Rate"
-              isGood={true}
-              isPercentage={true}
-              value={3}
-              valueChange={5}
-              // valueOutOf={}
-              // valueUnits=""
-            />
+                <AnalyticCard
+                  title="Turnovers"
+                  isGood={false}
+                  isPercentage={true}
+                  value={12}
+                  valueChange={8}
+                  // valueOutOf={}
+                  // valueUnits=""
+                />
 
-            <AnalyticCard
-              title="Happiness Score"
-              isGood={true}
-              isPercentage={false}
-              value={89}
-              valueChange={1}
-              valueOutOf={100}
-              valueUnits="Points"
-            />
-          </div>
+                <AnalyticCard
+                  title="Completion Rate"
+                  isGood={true}
+                  isPercentage={true}
+                  value={3}
+                  valueChange={5}
+                  // valueOutOf={}
+                  // valueUnits=""
+                />
 
+                <AnalyticCard
+                  title="Happiness Score"
+                  isGood={true}
+                  isPercentage={false}
+                  value={89}
+                  valueChange={1}
+                  valueOutOf={100}
+                  valueUnits="Points"
+                />
+              </div>
 
-
-
-
-
-
-
-          <div styleName="panel">
-    				<div styleName="title">Daily Active Users </div>
-            <div styleName="count"> {analytics.userCount} </div>
-    			</div>
-          <div styleName="panel">
-    				<div styleName="title">Pillars </div>
-            <div styleName="count"> {analytics.pillarCount} </div>
-    			</div>
-          <div styleName="panel">
-    				<div styleName="title">Total Points </div>
-            <div styleName="count"> {analytics.totalPoints} </div>
-    			</div>
-          <div styleName="panel">
-    				<div styleName="title">Content </div>
-            <div styleName="count"> {analytics.contentCount} </div>
-    			</div>
-          <div styleName="panel">
-    				<div styleName="title">Managers </div>
-            <div styleName="count"> {analytics.managerCount} </div>
-    			</div>
-          <div styleName="panel">
-    				<div styleName="title">Tenants </div>
-            <div styleName="count"> {analytics.tenantCount} </div>
-    			</div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <h1>Loading...</h1>
-        </div>
-      );
-    }
-
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <h1>Loading...</h1>
+            </div>
+          );
+        }
 	}
 }
 
