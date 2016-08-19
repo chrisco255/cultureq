1d909e65db617505ff74c53772d5d83a86b809db
Alicia Rodriguez <rodriguez.aliciaf@gmail.com>
2016-08-19 18:18:14 -0400

    added the content cards in seperate lists in reverse ordr, more prettifing-ish

--- src/routes/content/ContentPage.container.js
+++ src/routes/content/ContentPage.container.js
@@ -139,7 +139,8 @@ class ContentPage extends Component {
 
 	render() {
 
-    let listContents;
+    let listQuoteContents = [];
+    let listVideoContents = [];
 
     const activeContents = this.props.contents.filter((content) => !content.isDeleted);
     const contentViewOrder = {display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around'};
@@ -152,7 +153,7 @@ class ContentPage extends Component {
     }
 
     if(activeContents.length > 0) {
-      listContents = this.props.contents.map((content, index) => {
+      this.props.contents.forEach((content, index) => {
 
         let pillarName = content.pillarId;
         const pillarNameIndex = _.findIndex(this.props.pillars, (pillar) => pillar._id === content.pillarId);
@@ -161,22 +162,26 @@ class ContentPage extends Component {
         }
 
         if(!content.isDeleted) {
-          return (
+          listQuoteContents.push(
               <div>
                 { content.type === ContentTypes.QUOTE &&
                   <Quote key={content._id} data={content.data} index={index} pillarName={pillarName} deleteContent={this.props.deleteContent} content={content} editContent={this.props.editContent} isEditing={this.props.isEditing} contentThatIsBeingEdited={this.props.contentThatIsBeingEdited} contentThatIsBeingEditedIndex={this.props.contentThatIsBeingEditedIndex} finishEdit={this.props.finishEdit}
                   authorChangeContent={this.props.authorChangeContent} quoteChangeContent={this.props.quoteChangeContent} /> }
+              </div>
+  				);
+
+          listVideoContents.push(
+              <div>
                 { content.type === ContentTypes.VIDEO &&
                   <Video key={content._id} data={content.data} index={index} pillarName={pillarName} deleteContent={this.props.deleteContent} content={content} editContent={this.props.editContent} isEditing={this.props.isEditing} contentThatIsBeingEdited={this.props.contentThatIsBeingEdited} contentThatIsBeingEditedIndex={this.props.contentThatIsBeingEditedIndex} titleChangeContent={this.props.titleChangeContent}
                          descriptionChangeContent={this.props.descriptionChangeContent}
                          finishEdit={this.props.finishEdit}
                          urlChangeContent={this.props.urlChangeContent} /> }
-                {/*{ content.type === 'LUNCH' &&
-                  <Action key={content._id} data={content.data} index={index} pillarName={pillarName} deleteContent={this.props.deleteContent} content={content} /> } */}
               </div>
-  				);
+          );
         }
 			});
+
     }
 
     let submitBtnClassName = 'btn-floating btn-large waves-effect waves-light green';
@@ -187,7 +192,7 @@ class ContentPage extends Component {
 		return (
       <div style={{display: 'flex'}}>
 
-      <div style={{backgroundColor: 'rgba(117, 117, 117, 0.04)', height: '100em', width: '23%'}}>
+      <div style={{backgroundColor: 'rgba(117, 117, 117, 0.04)', height: '100em', width: '15%'}}>
         <div>
           <h1 style={{padding: '2.1rem 0 1.68rem 15px', margin: 'auto'}}>Select Content Type</h1>
         </div>
@@ -208,24 +213,33 @@ class ContentPage extends Component {
           }
       </div>
 
-      <div style={{width: '77%', paddingTop: '2.1rem'}}>
-        <div className="container">
+      <div style={{width: '60%', paddingTop: '2.1rem'}}>
+        <div style={{margin: '0px 10% 0px 10%'}}>
           { (this.props.currentContentType === ContentTypes.VIDEO) && <VideoForm onSubmit={this.onContentSubmit} pillars={this.props.pillars} type={this.props.currentContentType} /> }
           { (this.props.currentContentType === ContentTypes.QUOTE) && <QuoteForm onSubmit={this.onContentSubmit} pillars={this.props.pillars} type={this.props.currentContentType} /> }
           { (this.props.currentContentType === ContentTypes.RICHTEXT) &&
           <div>
             <h1>Write an Article</h1>
             <TextEditor onAutosave={ (rawContent) => this.onChange(rawContent) } />
-            <h1>Preview</h1>
+            { this.state.rawState && <h1>Preview</h1> }
             { this.state.rawState && <TextEditor readOnly={true} startingEditorState={this.state.rawState} /> }
-          </div> }
-          { (this.props.currentContentType === ContentTypes.RICHTEXT) && <div className="fixed-action-btn" style={{bottom: '45px', right: '24px'}}>
-            <a className={submitBtnClassName} onClick={this.onRichtextSubmit}>
-              <i className="large material-icons">check</i>
-            </a>
+            <div className="fixed-action-btn" style={{bottom: '45px', right: '24px'}}>
+              <button className={submitBtnClassName} onClick={this.onRichtextSubmit}>
+                <i className="large material-icons">check</i>
+              </button>
+            </div>
           </div> }
         </div>
       </div>
+
+      { (this.props.currentContentType === ContentTypes.VIDEO) && <div style={{width: '25%', paddingTop: '2.1rem'}}>
+        <h1>Video Content</h1>
+        {listVideoContents.reverse()}
+      </div> }
+      { (this.props.currentContentType === ContentTypes.QUOTE) && <div style={{width: '25%', paddingTop: '2.1rem'}}>
+        <h1>Quote Content</h1>
+        {listQuoteContents.reverse()}
+      </div> }
 
     </div>
 		);
